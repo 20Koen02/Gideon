0df699509e928ee26932368f8aed52b9fee274ea
Kolkies <stijn.vdkolk@icloud.com>
2018-01-31 13:24:38 +0100

    Nieuwe arg in weer command

--- commands/general/weer.js
+++ commands/general/weer.js
@@ -18,13 +18,19 @@ module.exports = class helpCommand extends Command {
                     key: 'city',
                     prompt: 'Welke plaats?',
                     type: 'string'
+                },
+                {
+                key: 'taal',
+                prompt: 'Welke taal wil je het weer hebben?',
+                type: 'string',
+                default: 'nl'
                 }
             ]
         });
     }
 
     async run(msg, args) {
-        const url = `http://wttr.in/${args.city.replace(' ', '%20')}_0tqp_lang=nl.png`;
+        const url = `http://wttr.in/${args.city.replace(' ', '%20')}_0tqp_lang=${args.taal}.png`;
         snekfetch.get(url).then(r=>msg.channel.send("", {files:[{attachment: r.body}]}));
 
       console.log(`Weer command gebruikt, ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb Geheugen`);
